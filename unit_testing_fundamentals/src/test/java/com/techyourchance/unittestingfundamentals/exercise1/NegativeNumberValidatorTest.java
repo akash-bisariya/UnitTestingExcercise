0e37f26403964b91d0f5191a71e5fe43eeb242e3
Akash bisariya <akash.bisariya@nagarro.com>
2021-05-21 00:03:18 +0530

    Unit test case for negative number

--- unit_testing_fundamentals/src/test/java/com/techyourchance/unittestingfundamentals/exercise1/NegativeNumberValidatorTest.java
+++ unit_testing_fundamentals/src/test/java/com/techyourchance/unittestingfundamentals/exercise1/NegativeNumberValidatorTest.java
@@ -1,5 +1,40 @@
 package com.techyourchance.unittestingfundamentals.exercise1;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+
 public class NegativeNumberValidatorTest {
+
+    NegativeNumberValidator negativeNumberValidator;
+
+    @Before
+    public void setup(){
+        negativeNumberValidator = new NegativeNumberValidator();
+    }
+
+    @Test
+    public void negative_negativeNumber_trueReturned(){
+        Assert.assertThat(negativeNumberValidator.isNegative(-1),is(true));
+    }
+
+    @Test
+    public void negative_negativeNumber1_trueReturned(){
+        Assert.assertThat(negativeNumberValidator.isNegative(-22),is(true));
+    }
+
+    @Test
+    public void negative_positiveNumber_falseReturned(){
+        Assert.assertThat(negativeNumberValidator.isNegative(0),is(false));
+    }
+
+    @Test
+    public void negative_positiveNumber1_falseReturned(){
+        Assert.assertThat(negativeNumberValidator.isNegative(10000),is(false));
+    }
+
+
 
 }
