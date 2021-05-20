e4a27b10152ace7fff4ce7c38c2f2bb3e949c7cd
Akash bisariya <akash.bisariya@nagarro.com>
2021-05-21 00:03:47 +0530

    Unit test case for StringDuplicator

--- unit_testing_fundamentals/src/test/java/com/techyourchance/unittestingfundamentals/exercise2/StringDuplicatorTest.java
+++ unit_testing_fundamentals/src/test/java/com/techyourchance/unittestingfundamentals/exercise2/StringDuplicatorTest.java
@@ -1,5 +1,36 @@
 package com.techyourchance.unittestingfundamentals.exercise2;
 
+import org.hamcrest.CoreMatchers;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertThat;
+
 public class StringDuplicatorTest {
+    
+    StringDuplicator stringDuplicator;
+    
+    @Before
+    public void setup() {
+        stringDuplicator = new StringDuplicator();
+    }
+
+    @Test
+    public void duplicate_emptyString_emptyStringReturned() {
+        assertThat(stringDuplicator.duplicate(""), CoreMatchers.is(""));
+    }
+
+    @Test
+    public void duplicate_smallString_duplicateStringReturned() {
+        assertThat(stringDuplicator.duplicate("abc"),CoreMatchers.is("abcabc"));
+    }
+
+    @Test
+    public void duplicate_longString_duplicateStringReturned() {
+        assertThat(stringDuplicator.duplicate("AkashBisariya"),CoreMatchers.is("AkashBisariyaAkashBisariya"));
+    }
+
+
 
 }
